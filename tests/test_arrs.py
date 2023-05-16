bbf627ba6a4542c988251dc84d4b0bb53d38ba86
Selim Seidov <selimseidov@gmail.com>
2023-05-16 20:39:57 +0300

    Покрытие 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,19 @@
+import pytest
 from utils import arrs
 
+@pytest.fixture()
+def date():
+    return [1, 2, 3]
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+def test_get(date):
+    assert arrs.get(date, 1, "test") == 2
+    with pytest.raises(IndexError):
+        assert arrs.get([], 0, "test") == "test"
+    assert arrs.get(date, -1, 'test') == 'test'
 
-
-def test_slice():
+def test_slice(date):
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice(date, 1) == [2, 3]
+    assert arrs.my_slice([], 1) == []
+    assert arrs.my_slice(date, -4) == date
+    assert arrs.my_slice(date, -2) == [2, 3] [2, 3]
